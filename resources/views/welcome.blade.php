88d2333885d5e3b267e8f54bf6387b7210ef67ee
Wahyu Adhe Triwibowo <adhedewa1996@gmail.com>
2020-10-05 11:18:31 +0700

    update fitur 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel X
                 </div>
 
                 <div class="links">
